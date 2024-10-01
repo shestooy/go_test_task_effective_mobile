e67760e3928d47854952e6cb702265853c33dfb2
shestooy <km.km.2021@inbox.ru>
2024-10-01 09:18:05 +0300

    remove custom UnMarshal

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,36 +1,10 @@
 package model
 
-import (
-	"encoding/json"
-	"time"
-)
-
 type Song struct {
-	ID          int       `json:"id,omitempty"  example:"1"`
-	Group       string    `json:"group" validate:"required" example:"Muse"`
-	Song        string    `json:"song" validate:"required" example:"Supermassive Black Hole"`
-	ReleaseDate time.Time `json:"releaseDate,omitempty" example:"2006-07-16"`
-	Text        string    `json:"text,omitempty" example:"Ooh baby, don't you know I suffer..."`
-	Link        string    `json:"link,omitempty" example:"https://www.youtube.com/watch?v=Xsp3_a-PMTw"`
-}
-
-func (s *Song) UnmarshalJSON(data []byte) error {
-	type Alias Song
-	aux := &struct {
-		ReleaseDate string `json:"releaseDate,omitempty"`
-		*Alias
-	}{
-		Alias: (*Alias)(s),
-	}
-	if err := json.Unmarshal(data, &aux); err != nil {
-		return err
-	}
-	if aux.ReleaseDate != "" {
-		t, err := time.Parse("2006-01-02", aux.ReleaseDate)
-		if err != nil {
-			return err
-		}
-		s.ReleaseDate = t
-	}
-	return nil
+	ID          int    `json:"id,omitempty"  example:"1"`
+	Group       string `json:"group,omitempty" validate:"required" example:"Muse"`
+	Song        string `json:"song,omitempty" validate:"required" example:"Supermassive Black Hole"`
+	ReleaseDate string `json:"releaseDate,omitempty" example:"2006-07-16"`
+	Text        string `json:"text,omitempty" example:"Ooh baby, don't you know I suffer..."`
+	Link        string `json:"link,omitempty" example:"https://www.youtube.com/watch?v=Xsp3_a-PMTw"`
 }
